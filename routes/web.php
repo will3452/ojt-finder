9ed05061b0f457b233dc707df3a28080d4e6c368
William Galas <william.galas@coreproc.ph>
2021-10-25 16:48:33 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ use App\Http\Controllers\ResumeController;
 
 Route::redirect('/', 'login');
 
-Auth::routes();
+Auth::routes(['verify' => true]);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
