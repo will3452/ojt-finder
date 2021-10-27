e9d5e31d07a0448baf5f1c45d3729669d90d4f9d
William Galas <william.galas@coreproc.ph>
2021-10-27 11:45:22 +0800

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -35,7 +35,7 @@ class User extends Authenticatable implements MustVerifyEmail
 
     public function getNameAttribute()
     {
-        return $this->first_name.', '. $this->middle_name.', '.$this->last_name;
+        return $this->first_name.' '. $this->middle_name.' '.$this->last_name;
     }
 
     /**
